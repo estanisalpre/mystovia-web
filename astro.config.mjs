9968a2d6084a11263511bfda3c0a9633b2fc59ab
Estanislao Salinas <estanisprevite@hotmail.com>
2025-11-04 17:38:05 -0500

    feat: add missing import for path module in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
+import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
